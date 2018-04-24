09d78c469e272c27fff367315901c5b27ddcaff6
KnoeBert <KnoeBert@users.noreply.github.com>
2018-04-24 18:55:23 +0200

    Grundgerüst Text

--- src/fingerprinting/Fingerprinting.java
+++ src/fingerprinting/Fingerprinting.java
@@ -15,7 +15,8 @@ public class Fingerprinting {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Kommentar
+        main m = new main();
+        m.setVisible(true);
     }
     
 }
